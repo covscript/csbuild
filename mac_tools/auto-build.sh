f25b41f76ab97ba8ea6768a5ce8d77cf8106c635
Michael Lee <mikecovlee@163.com>
2019-06-16 16:58:42 +0800

    Finsh packaging scripts

--- mac_tools/auto-build.sh
+++ mac_tools/auto-build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-bash ../build.sh
-bash ../install.sh
+#bash ../build.sh
+#bash ../install.sh
 bash ./make-app.sh ./build ./app_icon.png
+bash ./make-dmg.sh ./CovScript.app
+version_prefix=`./build/bin/cs -v | grep "^Version: " | awk '{print $2}'`
+version_suffix=`./build/bin/cs -v | grep "^Version: " | awk '{print $NF}'`
+mv covscript.dmg covscript-${version_prefix}"."${version_suffix}.dmg
+rm -r CovScript.app
