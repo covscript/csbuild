8c572af969889fb818307bb1cc88db80853d21ba
Michael Lee <mikecovlee@163.com>
2019-06-16 21:10:39 +0800

    Support background image

--- mac_tools/auto-build.sh
+++ mac_tools/auto-build.sh
@@ -2,7 +2,7 @@
 bash ../build.sh
 bash ../install.sh
 bash ./make-app.sh ./build ./app_icon.png
-bash ./make-dmg.sh ./CovScript.app
+bash ./make-dmg.sh ./CovScript.app ./app_bg.png
 version_prefix=`./build/bin/cs -v | grep "^Version: " | awk '{print $2}'`
 version_suffix=`./build/bin/cs -v | grep "^Version: " | awk '{print $NF}'`
 mv covscript.dmg covscript-${version_prefix}"."${version_suffix}.dmg
