1cd576db7c79bd190a495d946b4aba4fc6f0afb3
Michael Lee <mikecovlee@163.com>
2019-08-24 15:03:57 +0800

    Update step4_pack.bat

--- win_tools/step4_pack.bat
+++ win_tools/step4_pack.bat
@@ -1,4 +1,9 @@
 @echo off
 cd builds
+del *.7z
 7z a -mmt4 -mx9 build.7z build
 7z a -mmt4 -mx9 build_x64.7z build_x64
+7z a -mmt4 -mx9 cs_dev.7z build\include build\lib
+rd /S /Q build\include build\lib
+7z a -mmt4 -mx9 cs_dev_x64.7z build_x64\include build_x64\lib
+rd /S /Q build_x64\include build_x64\lib
