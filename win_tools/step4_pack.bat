143bd5f420e2c9d1038d22c207098ab6bc42039e
Michael Lee <mikecovlee@163.com>
2020-12-04 15:59:11 +0800

    remove i386 support

--- win_tools/step4_pack.bat
+++ win_tools/step4_pack.bat
@@ -1,9 +1,9 @@
 @echo off
 cd builds
 del *.7z
-7z a -mmt4 -mx9 covscript-win32.7z build
+::7z a -mmt4 -mx9 covscript-win32.7z build
 7z a -mmt4 -mx9 covscript-win64.7z build_x64
-7z a -mmt4 -mx9 cs_dev.7z build\include build\lib
-rd /S /Q build\include build\lib
+::7z a -mmt4 -mx9 cs_dev.7z build\include build\lib
+::rd /S /Q build\include build\lib
 7z a -mmt4 -mx9 cs_dev_x64.7z build_x64\include build_x64\lib
 rd /S /Q build_x64\include build_x64\lib
