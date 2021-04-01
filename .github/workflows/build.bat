1c696609acf66f8434c8abcb713b3549865285b3
李登淳 <lee@covariant.cn>
2021-04-01 18:41:12 +0800

    Update build.bat

--- .github/workflows/build.bat
+++ .github/workflows/build.bat
@@ -2,7 +2,7 @@
 cd %~dp0
 7z x ..\..\gcc-10.2.0-mingw-w64-8.0.0-r8-covscript.org.zip
 7z x ..\..\wix311-binaries.zip -owix
-set /M PATH=%cd%\wix;%cd%\mingw-w64\bin;%PATH%
+setx /M PATH %cd%\wix;%cd%\mingw-w64\bin;%PATH%
 echo %PATH%
 cd ..\..\
 .\auto-build.bat
