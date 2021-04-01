594bcda1e0425fcbc0f197d08d13ac556387828e
李登淳 <lee@covariant.cn>
2021-04-01 11:08:35 +0800

    finish generate msi file

--- package_tools/wix/gen.bat
+++ package_tools/wix/gen.bat
@@ -1,3 +1,2 @@
-@echo off
-cd %~dp0
+echo %~dp0
 heat dir ..\..\build\ -cg Package -gg -sfrag -template:fragment -out temp.wxs
--- package_tools/wix/make.bat
+++ package_tools/wix/make.bat
@@ -1,4 +1,11 @@
 @echo off
 cd %~dp0
-candle Product.wxs
-light -ext WixUIExtension -cultures:en-us Product.wixobj -out CovScript.msi
+xcopy /Y license.rtf ..\..\build
+mkdir wix_build
+cd wix_build
+candle ..\Product.wxs -nologo
+light -ext WixUIExtension -b ..\..\..\build -cultures:en-us Product.wixobj -out CovScript.msi -nologo
+xcopy /Y CovScript.msi ..\..\..
+cd ..
+rd /S /Q .\wix_build
+del /Q ..\..\build\license.rtf
