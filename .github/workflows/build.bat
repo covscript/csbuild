96c31daec270fc5bb3449e2d2f13f1f05818e80f
李登淳 <lee@covariant.cn>
2021-04-01 17:32:13 +0800

    fix win32 build

---
+++ .github/workflows/build.bat
@@ -0,0 +1,9 @@
+@echo off
+cd %~dp0
+7z x ..\..\gcc-10.2.0-mingw-w64-8.0.0-r8-covscript.org.zip
+7z x ..\..\wix311-binaries.zip -owix
+set PATH=%cd%\wix;%cd%\mingw-w64\bin;%PATH%
+echo %PATH%
+cd ..\..\
+.\auto-build.bat
+.\package_tools\dmg\make.bat
