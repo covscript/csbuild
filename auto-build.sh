8cec59aa4ed88d1955a531259f386ce6c1f49b11
李登淳 <mikecovlee@163.com>
2022-12-14 18:54:56 +0800

    Update auto-build.sh

--- auto-build.sh
+++ auto-build.sh
@@ -19,4 +19,4 @@ cp -rf covscript-process/build ..
 
 cd ..
 ./build/bin/cs -i ./build/imports ./misc/cspkg_build.csc ./misc/cspkg_config.json
-cp -rf build-cache/ecs/build ./build
+cp -rf build-cache/ecs/build .
