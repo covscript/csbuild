8337ebb4038aa8d9e4fd65605746649ec19cffe4
李登淳 <mikecovlee@163.com>
2023-06-05 15:15:38 +0800

    update unix build scripts

--- auto-build.sh
+++ auto-build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+function start ()
+{
+    ../build/bin/cs -i ../build/imports ../misc/auto_build.csc $1
+}
+
 CURRENT_FOLDER=$(dirname $(readlink -f "$0"))
 export CS_DEV_PATH=${CURRENT_FOLDER}/build-cache/covscript/csdev
 cd $CURRENT_FOLDER
@@ -11,19 +16,35 @@ else
     CSPKG_CONFIG="./misc/cspkg_nightly_config.json"
 fi
 
-bash ./misc/unix_build.sh $1
+bash ./misc/unix_build_minimal.sh $1
 
 rm -rf ./build
 mkdir -p build/bin
 cd build-cache
 
-cp -rf cspkg/build ..
-cp -rf covscript/build ..
-cp -rf covscript/csdev/* ../build/
-cp -rf covscript-curl/build ..
-cp -rf covscript-regex/build ..
-cp -rf covscript-codec/build ..
-cp -rf covscript-process/build ..
+# Setup basic environment
+
+cp -rf cspkg/build .. &
+cp -rf covscript/build .. &
+cp -rf covscript/csdev/* ../build/ &
+cp -rf covscript-curl/build .. &
+cp -rf covscript-regex/build .. &
+cp -rf covscript-codec/build .. &
+cp -rf covscript-process/build .. &
+wait
+
+# Concurrent works
+
+start ecs &
+start stdutils &
+start covanalysis &
+start covscript-darwin &
+start covscript-sqlite &
+start covscript-network &
+start covscript-imgui &
+start covscript-zip &
+start covscript-database &
+wait
 
 cd ..
 ./build/bin/cs -i ./build/imports ./misc/cspkg_build.csc $CSPKG_CONFIG
