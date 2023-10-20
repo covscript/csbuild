cec823fa1a50f90e6a8caee5fbb22b7c9dea06ae
Michael Lee <mikecovlee@163.com>
2023-10-20 10:34:14 +0800

    fix wix toolsets

--- .github/workflows/build.bat
+++ .github/workflows/build.bat
@@ -1,6 +1,6 @@
 @echo off
 cd %~dp0
-set PATH=C:\mingw64-covscript\mingw64\bin;%PATH%
+set PATH=C:\mingw64-covscript\mingw64\bin;C:\Program Files (x86)\WiX Toolset v3.11\bin;%PATH%
 cd ..\..
 call .\auto-build.bat
 call .\package_tools\wix\make.bat
